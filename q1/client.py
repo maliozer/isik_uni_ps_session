c0628166524a472203fc497128968f9397560b33
maliozer <mehmetali.ozer@std.yeditepe.edu.tr>
2020-11-15 05:45:43 +0300

    tcp_ip client server messaging

---
+++ q1/client.py
@@ -0,0 +1,20 @@
+import socket
+
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('127.0.0.1', 10002)
+
+print('connecting to %s port %s' % server_address)
+
+sock.connect(server_address)
+#%%
+
+for x in range(3):
+    message = 'connection done' + str(x) + 'th time from ' + str(ip_address) + '\n'
+    byt = message.encode()
+    sock.send(byt)
---
+++ q1/main.py
@@ -0,0 +1,22 @@
+import socket
+
+#create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#bind the socket to the port 10000
+server_address = ('localhost', 10002)
+
+print('starting up on %s port %s' % server_address)
+sock.bind(server_address)
+#sock.close() to stop
+
+sock.listen(1)
+
+#%%
+while True:
+    conn, addr = sock.accept()
+    data = conn.recv(2000)
+    print(data.decode())
+    
+    if(data.decode() == 'exit -n'):
+        break

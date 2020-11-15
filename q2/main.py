61a3e1974ac314a431f487c689f1023227a243dc
maliozer <mehmetali.ozer@std.yeditepe.edu.tr>
2020-11-15 06:56:52 +0300

    mysql_operations

---
+++ q2/main.py
@@ -0,0 +1,44 @@
+#mysql connection problem from python
+'''
+sudo mysql
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+
+'''
+
+### use this -> !pip install mysql-connector
+import mysql.connector
+
+#define host, root,passwd,database inline
+def mysql_connector():
+    db_con = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        passwd="root",
+    )
+    return db_con
+
+mysql_con = mysql_connector()
+
+mysql_cursor = mysql_con.cursor()
+
+mysql_cursor.execute("CREATE DATABASE example1")
+mysql_con.database = 'example1'
+
+#%%
+mysql_cursor.execute("CREATE TABLE Exam (StudentID VARCHAR(11), CourseID VARCHAR(11), Grade INT)")
+#%%
+
+sql = "INSERT INTO Exam (StudentID, CourseID, Grade) VALUES (%s, %s, %s)"
+val = ("ALi", "Özer", "77")
+mysql_cursor.execute(sql, val)
+mysql_con.commit()
+print(mysql_cursor.rowcount, "record inserted.")
+
+#%%
+#show results
+mysql_cursor.execute("SELECT * FROM Exam")
+myresult = mysql_cursor.fetchall()
+
+for x in myresult:
+  print(x)
